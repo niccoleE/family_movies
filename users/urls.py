2e49be8c51be69f547ed7b273d6744f450ac87a2
Olha <niccole.au@gmail.com>
2022-06-13 18:38:25 +0300

    login page error detect

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('login_check/', views.login_check, name='login_check'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -15,11 +16,30 @@ def register(request):
             new_user = form.save()
             login(request, new_user)
             return redirect('my_movies:home')
-        else:
-            for text in form.errors.values():
-                print(text)
-
-
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+
+def login_check(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        check_username = request.POST['username']
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("my_movies:home")
+        else:
+            if User.objects.filter(username=check_username).exists():
+                print("exists")
+                messages.error(request, "Invalid password.")
+            else:
+                messages.error(request, "No such user. Please check the username"
+                                        " or register if you haven't registered yet")
+                return redirect("users:login_check")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "registration/login.html", context)
